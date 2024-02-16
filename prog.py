7523e121825e6928b14c4a7292d9c002d8cbf364
VladimirMosh <the.uncle.mosh@gmail.com>
2024-02-16 18:06:26 +0300

    testCommit

---
+++ prog.py
@@ -0,0 +1,22 @@
+
+import datetime
+
+
+notesfile = open("notes.txt", "w")
+notes = {} 
+dates = datetime
+
+notetext = str(input('Введите тело заметки:    '))
+notetitle = str(input('Введите заголовок заметки:    '))
+note = {                        
+    'Notetitle': notetitle,
+    'Dates': dates,
+    'Text': notetext,
+}
+def create_note(): {}     
+def save_note():{}          
+def all_notes():{}            
+def filter(): {}          
+def delete_note(): {}    
+def redact_note(): {}     
+def menu(): {}           
